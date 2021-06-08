57512c8bf18829594270127251bd5ee9f4ad80b9
MaryamPayenda <maryampayanda12@gmail.com>
2021-06-08 13:58:50 +0200

    add

--- src/components/Country.js
+++ src/components/Country.js
@@ -2,13 +2,14 @@ import React from "react";
 
 const Country = ({ results }) => {
   const lands = results.map((obj, i) => {
-    const { name, flag, capital, population } = obj;
+    const { name, flag, capital, population, language } = obj;
     return (
-      <div key={i}>
-        <h2>{name}</h2>
-        <h5>{capital}</h5>
-        <h3>{population}</h3>
-        <img src={flag} alt="{name}" />
+      <div key={i} className="container">
+        <h2> {name}</h2>
+        <h5>Capital :{capital}</h5>
+        <h3> Population: {population}</h3>
+        <h5>{language}</h5>
+        <img src={flag} alt="{name}" className="flag" />
       </div>
     );
   });
